927979eeca839766dcbe64f1af788f8cda0804f9
Ussama Jaffar <Ussama.jaffar@gmail.com>
2023-10-19 12:43:45 +0500

    added the app into the installed app's

--- Event/settings.py
+++ Event/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user'
+    'user',
+    "rest_framework_simplejwt.token_blacklist",
+    "rest_framework.authtoken"
 ]
 
 MIDDLEWARE = [
